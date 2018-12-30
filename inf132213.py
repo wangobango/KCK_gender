6c74f1e791fd3d88ee3547cba2eed6d817fe8380
wangobango <puch003@wp.pl>
2018-12-30 18:05:44 +0100

    dupa2

--- inf132213.py
+++ inf132213.py
@@ -12,32 +12,77 @@ import sys
 import subprocess
 
 # from ipywidgets import *
-import scipy.io.wavfile
+import scipy.signal as ss
 import soundfile as sf
-# from soundfile import *
-import pyaudio as pa
-
 
 def read_wave(name):
-    fs, dat = scipy.io.wavfile.read(name)
-    # print(dat.shape)
-    # return [d[0] for d in dat]
-    return dat
+    name = './trainall/'+name
+    data, samplerate = sf.read(name)
+    return np.asarray(data), samplerate
 
+def decimate(wave):
+    temp = np.fft.fft(wave)
+    return np.asarray(ss.decimate(temp,13,zero_phase = False))
 
 if __name__ == "__main__":
-    # s = pd.AudioSegment.from_file("/train/trainall/001_K.wav", format="wav")
-    # w = wave.open('./train/trainall/001_K.wav', 'r')
-    # for i in range(w.getnframes()):
-    #     frame = w.readframes(i)
-    #     print(frame)
+    if(sys.argv[1] == 'test'):
+        files = os.listdir('./trainall')
+        males = 0
+        females = 0
+        errors = 0
+        allV = len(files)
+        for item in files:
+            data, rate = read_wave(item)
+            signals = []
+            signals.append(data)
+            for i in range(7):
+                data = decimate(data)
+                signals.append(data)
 
-    # w, signal = scipy.io.wavfile.read('./train/trainall/001_K.wav')
-    # print(signal)
+            valueErr = False
 
-    
-    # data, samplerate = sf.read('./train/trainall/002_M.wav')
-    # print(samplerate);
-    data, samplerate = sf.read('./train/trainall/003_K.wav')
-    print(samplerate);
-    # read_wave('./train/trainall/002_M.wav');
+            for i in range(1,7):
+                try:
+                    signals[0] = np.multiply(signals[0],signals[i])
+                except ValueError:
+                    print("VALUE ERROR")
+                    errors+=1
+                    valueErr = True
+                    break
+
+            if(not(valueErr)):
+                result = np.argmax(signals[0])/rate
+                if(result < 2 and 'M' in item):
+                    males+=1
+                elif(result > 2 and 'K' in item):
+                    females +=1
+                # print(np.argmax(signals[0])/rate)
+        
+        print((males+females)/allV)
+        print(males)
+        print(females)
+        print(allV)
+        print(errors)
+
+    else:
+        data, rate = read_wave(sys.argv[1])
+        signals = []
+        signals.append(data)
+        for i in range(7):
+            data = decimate(data)
+            signals.append(data)
+
+        valueErr = False
+
+        for i in range(1,7):
+            try:
+                signals[0] = np.multiply(signals[0],signals[i])
+            except ValueError:
+                print("VALUE ERROR")
+                valueErr = True
+                break
+
+        if(not(valueErr)):
+            print(np.argmax(signals[0])/rate)
+
+   
